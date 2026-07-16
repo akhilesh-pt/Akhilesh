cbbd5e8fb388544b5238573f8b0736f90c237355
Akhilesh <akhi3245@gmail.com>
2026-07-16 10:18:22 +0200

    Added restaurant_bill.py

---
+++ Projects/restaurant_bill.py
@@ -0,0 +1,15 @@
+customer_name = input("Enter Customer Name: ")
+food_bill = int(input("Enter food bill: "))
+drinks_bill = int(input("Enter drinks bill: "))
+dessert_bill = int(input("Enter dessert bill: "))
+total_bill = food_bill + drinks_bill + dessert_bill
+
+print("Customer Nmae : ",customer_name)
+
+print("Food Bill = ",food_bill)
+
+print("Drinks Bill = ",drinks_bill)
+
+print("Dessert Bill = ",dessert_bill)
+
+print("Total Bill = ",total_bill)
